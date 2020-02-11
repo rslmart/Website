4ff1dee6c0591b9f1c5141b2f37cc33da12abea7
Makoa Martin <rslmart@amazon.com>
2020-02-11 17:32:02 -0500

    Working on mongo db, images on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
+const imageArray = ["https://www.nrlmry.navy.mil/tcdat/tc19/ATL/05L.DORIAN/ir/geo/1km/20190901.1300.goes16.x.ir1km.05LDORIAN.140kts-927mb-265N-765W.100pc.jpg",
+"https://www.nrlmry.navy.mil/tcdat/tc19/ATL/05L.DORIAN/ir/geo/1km/20190901.1700.goes16.x.ir1km.05LDORIAN.160kts-910mb-265N-771W.100pc.jpg"];
+
+var image;
+
+function arrayBufferToBase64(buffer) {
+  var binary = '';
+  var bytes = [].slice.call(new Uint8Array(buffer));
+
+  bytes.forEach((b) => binary += String.fromCharCode(b));
+
+  return window.btoa(binary);
+};
+
+function loadImage(imageUrl) {
+    fetch(proxyurl + imageUrl).then((response) => {
+      response.arrayBuffer().then((buffer) => {
+        var base64Flag = 'data:image/jpeg;base64,';
+        var imageStr = arrayBufferToBase64(buffer);
+        const img = document.createElement("img");
+        img.src = imageUrl;
+        document.getElementById('myImg');
+        // = base64Flag + imageStr
+      });
+    });
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+          {
+              imageArray.map(image =>
+                  (<img src={image}/>)
+              )
+          }
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
