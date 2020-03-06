6146eb6784851ff503b363954ccf1de543f0d182
Makoa Martin <rslmart@amazon.com>
2020-03-05 22:51:40 -0500

    Flask calls working, frontend now displays options and counts images that match. Working on grouping options by whether they match existing selections or not

--- Backend/app.py
+++ Backend/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import pymongo
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["mydatabase"]
@@ -13,14 +15,48 @@ ibtracsCol = mydb["ibtracs"]
 def hello_world():
     return 'Hello World!'
 
-@app.route('/images', methods=['GET'])
+@app.route('/images/imageCount', methods=['POST'])
+def imageCount():
+    query = request.get_json()
+    print(query)
+    return jsonify({'count': imagesCol.find(query).count()})
+
+@app.route('/images/allOptions', methods=['GET'])
+def imageAllOptions():
+    keys = ['season', 'basin', 'storm_number', 'storm_agency', 'storm_name', 'type', 'sensor', 'resolution', 'satellite', 'satellite']
+    options = {}
+    for key in keys:
+        options[key] = imagesCol.distinct(key)
+    return jsonify({'options': options})
+
+@app.route('/images/options', methods=['POST'])
+def imageOptions():
+    keys = ['season', 'basin', 'storm_number', 'storm_agency', 'storm_name', 'type', 'sensor', 'resolution', 'satellite', 'extension']
+    requestJson = request.get_json()
+    print(requestJson)
+    queryResult = imagesCol.find(requestJson["query"])
+    options = {}
+    for key in keys:
+        if key not in requestJson["keys"]:
+            options[key] = queryResult.distinct(key)
+    return jsonify({'options': options})
+
+@app.route('/images/query', methods=['POST'])
 def imageQuery():
-    print(request.get_json())
-    print(json.laods(request.get_json()))
+    requestJson = request.get_json()
+    print(requestJson)
+    query = requestJson["query"]
     output = []
-    for s in imagesCol.find(json.laods(request.get_json())):
+    count = 0
+    for s in imagesCol.find(query):
+        del s['_id']
+        del s['date']
         output.append(s)
-    return jsonify({'result': output})
+        count += 1
+        if count > requestJson["limit"]:
+             break
+    print(output)
+    return jsonify({'imageItems': output})
 
 
 if __name__ == '__main__':
--- frontend/src/Images/ImagePage.js
+++ frontend/src/Images/ImagePage.js
@@ -1,44 +1,50 @@
 import React, { Component } from "react";
 import TextField from "@material-ui/core/TextField";
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Autocomplete from "@material-ui/lab/Autocomplete";
 
 class ImagePage extends Component {
+    API_GATEWAY_ENDPOINT = "http://127.0.0.1:5000";
+
     keys = ['season', 'basin', 'storm_number', 'storm_agency', 'storm_name', 'type', 'sensor', 'resolution',
         'image_url', 'year', 'month', 'day', 'hour', 'minute', 'second', 'satellite', 'extension'];
 
-    imageOptions = {
-        seasons: Array.from({length: 2020-1997}, (v, k) => k+1997),
-        basins: ['ATL', 'CPAC', 'EPAC', 'IO', 'SHEM', 'WPAC']
-    };
+    seasonsOptions = Array.from({length: 2020-1997}, (v, k) => k+1997);
 
     state = {
         seasonSelections: [],
         basinSelections: [],
-        stormNameOptions: [],
-        typeOptions: [],
-        sensorOptions: [],
-        resolutionOptions: [],
-        satelliteOptions: []
+        storm_nameSelections: [],
+        typeSelections: [],
+        sensorSelections: [],
+        resolutionSelections: [],
+        satelliteSelections: [],
+        extensionSelections: [],
+        allImageOptions: {
+            basin: [],
+            storm_name: [],
+            type: [],
+            sensor: [],
+            resolution: [],
+            satellite: [],
+            extension: []
+        },
+        imageOptions: {
+            basin: [],
+            storm_name: [],
+            type: [],
+            sensor: [],
+            resolution: [],
+            satellite: [],
+            extension: []
+        },
+        count: -1
     };
 
-    getOptions = async (query) => {
-        
+    componentDidMount = async () => {
+        this.fetchOptions({}, []);
+        this.fetchImageCount({});
     };
-
-    /*
-    {
-        "$and": [
-            {
-                "$or": [
-                    {"season":1997}, {"season":1998}]
-            },
-            {
-                "$or": [
-                    {"basin":"ATL"}, {"basin":"WPAC"}]
-            }
-        ]
-    }
-     */
 
     inputChange = async (evt, value, id) => {
         switch (id) {
@@ -48,14 +54,114 @@ class ImagePage extends Component {
             case "basin":
                 await this.setState({ basinSelections: value});
                 break;
+            case "storm_name":
+                await this.setState({ storm_nameSelections: value});
+                break;
+            case "type":
+                await this.setState({ typeSelections: value});
+                break;
+            case "sensor":
+                await this.setState({ sensorSelections: value});
+                break;
+            case "resolution":
+                await this.setState({ resolutionSelections: value});
+                break;
+            case "satellite":
+                await this.setState({ satelliteSelections: value});
+                break;
+            case "extension":
+                await this.setState({ extensionSelections: value});
+                break;
             default:
                 break;
         }
-
         const seasonQuery = {"$or": this.state.seasonSelections.map(season => {return {"season":season}})};
         const basinQuery = {"$or": this.state.basinSelections.map(basin => {return {"basin":basin}})};
-        const query = {"$and": [seasonQuery, basinQuery]};
+        const storm_nameQuery = {"$or": this.state.storm_nameSelections.map(storm_name => {return {"storm_name":storm_name}})};
+        const typeQuery = {"$or": this.state.typeSelections.map(type => {return {"type":type}})};
+        const sensorQuery = {"$or": this.state.sensorSelections.map(sensor => {return {"sensor":sensor}})};
+        const resolutionQuery = {"$or": this.state.resolutionSelections.map(resolution => {return {"resolution":resolution}})};
+        const satelliteQuery = {"$or": this.state.satelliteSelections.map(satellite => {return {"satellite":satellite}})};
+        const extensionQuery = {"$or": this.state.extensionSelections.map(extension => {return {"extension":extension}})};
+        const queryList = [seasonQuery, basinQuery, storm_nameQuery, typeQuery, sensorQuery, resolutionQuery,
+            satelliteQuery, extensionQuery].filter(query => query["$or"].length > 0)
+        if (queryList.length > 0) {
+            const query = {"$and": queryList};
+            console.log(JSON.stringify(query));
+            this.fetchImageCount(query);
+        } else {
+            this.fetchImageCount({});
+        }
+    };
+
+    /*
+     * This function takes all of the key/value pairs of the "request" object and transforms them into url query
+     * parameters. So if you get a request object that looks like:
+     * { keyOne: valueOne, keyTwo: valueTwo },
+     * It will then transform them into a string of the form:
+     * keyOne=valueOne&keyTwo=valueTwo
+     */
+    getQueryParamsFromRequest = request => Object.keys(request).map(key => (`${key}=${request[key]}`)).join("&");
+
+    fetchImageCount = async (query) => {
+        this.setState({ count: -1});
         console.log(JSON.stringify(query));
+                    fetch(`${this.API_GATEWAY_ENDPOINT}/images/imageCount`, {
+            method: "POST",
+            body: JSON.stringify(query),
+            headers: {
+                "Content-Type": "application/json"
+            }
+            }).then((response) => {
+                return response.json();
+            }).then((data) => {
+                console.log(data);
+                this.setState({ count: data.count});
+            });
+    };
+
+    /**
+     * This fetchs options based on what has been selected so far
+     * @param query
+     * @returns {Promise<void>}
+     */
+    fetchOptions = async (query, keys) => {
+        console.log(query);
+        const request = {"query": query, "keys": keys};
+        console.log(request);
+        fetch(`${this.API_GATEWAY_ENDPOINT}/images/options`, {
+            method: "POST",
+            body: JSON.stringify(request),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then((response) => {
+            return response.json();
+        }).then((data) => {
+            this.setState({ imageOptions: Object.assign(this.state.imageOptions, data.options) });
+            if (query === {}) {
+                this.setState({ allImageOptions: data.options })
+            }
+        });
+    };
+
+    /**
+     * Call this when we finally want to search for images
+     * @param query
+     * @returns {Promise<void>}
+     */
+    fetchQuery = async (query, limit) => {
+        fetch(`${this.API_GATEWAY_ENDPOINT}/images/query`, {
+            method: "POST",
+            body: JSON.stringify({"query": query, "limit": limit}),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then((response) => {
+            return response.json();
+        }).then((data) => {
+            console.log(data)
+        });
     };
 
     render () {
@@ -63,7 +169,7 @@ class ImagePage extends Component {
             <Autocomplete
                 multiple
                 id="season"
-                options={this.imageOptions.seasons}
+                options={this.seasonsOptions}
                 getOptionLabel={option => option}
                 style={{width: 300}}
                 renderInput={params => (
@@ -77,6 +183,127 @@ class ImagePage extends Component {
                 )}
                 onChange={(evt, value) => this.inputChange(evt, value, "season")}
             />
+            <Autocomplete
+                multiple
+                id="basin"
+                options={this.state.imageOptions.basin}
+                getOptionLabel={option => option}
+                style={{width: 300}}
+                renderInput={params => (
+                    <TextField
+                        {...params}
+                        variant="standard"
+                        label="Basin"
+                        placeholder="ATL"
+                        fullWidth
+                    />
+                )}
+                onChange={(evt, value) => this.inputChange(evt, value, "basin")}
+            />
+            <Autocomplete
+                multiple
+                id="storm_name"
+                options={this.state.imageOptions.storm_name}
+                getOptionLabel={option => option}
+                style={{width: 300}}
+                renderInput={params => (
+                    <TextField
+                        {...params}
+                        variant="standard"
+                        label="Storm Name"
+                        placeholder="ISABEL"
+                        fullWidth
+                    />
+                )}
+                onChange={(evt, value) => this.inputChange(evt, value, "storm_name")}
+            />
+            <Autocomplete
+                multiple
+                id="type"
+                options={this.state.imageOptions.type}
+                groupBy={option => option.firstLetter}
+                getOptionLabel={option => option}
+                style={{width: 300}}
+                renderInput={params => (
+                    <TextField
+                        {...params}
+                        variant="standard"
+                        label="Type"
+                        placeholder=""
+                        fullWidth
+                    />
+                )}
+                onChange={(evt, value) => this.inputChange(evt, value, "type")}
+            />
+            <Autocomplete
+                multiple
+                id="sensor"
+                options={this.state.imageOptions.sensor}
+                getOptionLabel={option => option}
+                style={{width: 300}}
+                renderInput={params => (
+                    <TextField
+                        {...params}
+                        variant="standard"
+                        label="Sensor"
+                        placeholder=""
+                        fullWidth
+                    />
+                )}
+                onChange={(evt, value) => this.inputChange(evt, value, "sensor")}
+            />
+            <Autocomplete
+                multiple
+                id="resolution"
+                options={this.state.imageOptions.resolution}
+                getOptionLabel={option => option}
+                style={{width: 300}}
+                renderInput={params => (
+                    <TextField
+                        {...params}
+                        variant="standard"
+                        label="Resolution"
+                        placeholder=""
+                        fullWidth
+                    />
+                )}
+                onChange={(evt, value) => this.inputChange(evt, value, "resolution")}
+            />
+            <Autocomplete
+                multiple
+                id="satellite"
+                options={this.state.imageOptions.satellite}
+                getOptionLabel={option => option}
+                style={{width: 300}}
+                renderInput={params => (
+                    <TextField
+                        {...params}
+                        variant="standard"
+                        label="Satellite"
+                        placeholder=""
+                        fullWidth
+                    />
+                )}
+                onChange={(evt, value) => this.inputChange(evt, value, "satellite")}
+            />
+            <Autocomplete
+                multiple
+                id="extension"
+                options={this.state.imageOptions.extension}
+                getOptionLabel={option => option}
+                style={{width: 300}}
+                renderInput={params => (
+                    <TextField
+                        {...params}
+                        variant="standard"
+                        label="Extension"
+                        placeholder="jpg, png"
+                        fullWidth
+                    />
+                )}
+                onChange={(evt, value) => this.inputChange(evt, value, "extension")}
+            />
+            <span>Count {this.state.count !== -1 ? this.state.count : <CircularProgress/>}</span>
         </div>);
     }
 }
