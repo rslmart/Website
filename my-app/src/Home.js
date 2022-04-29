f1374368e7c160a869481a7fddd489acfd810b06
Makoa Martin <rslmart@amazon.com>
2022-04-29 10:28:49 +0100

    Adding multiple pages. Home page has background and links.

---
+++ my-app/src/Home.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {Link } from "react-router-dom";
+import pic from "./Pics/Me_Matterhorn.png";
+import './index.css';
+
+function home() {
+
+    return (
+        <div className={"app"}>
+            <img src={pic} alt="logo"/>
+            <Link to="/bio">
+                <button
+                    className={"tile1"}
+                >
+                    Bio
+                </button>
+            </Link>
+            <Link to="/bio">
+                <button
+                    className={"tile2"}
+                >
+                    Travels
+                </button>
+            </Link>
+        </div>
+    );
+
+}
+
+export default home;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import Home from "./Home";
+import Bio from "./Bio";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+    <BrowserRouter>
+        <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route path="/Bio" element={<Bio/>} />
+        </Routes>
+    </BrowserRouter>,
+    rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
